1202b3998105b7d1e028e967256978b354c589b9
UMGProyect <atreyuznight@gmail.com>
2024-07-27 12:51:20 -0600

    Comentario en la Conexion BD.

--- ContextBD/BD.cs
+++ ContextBD/BD.cs
@@ -2,6 +2,7 @@
 {
     public class BD
     {
+        //Conexion a la Base de datos SQL
         public static string connectionString = "Server=DESKTOP-RCOQL3Q; Initial Catalog = Telecomunicaciones; Trusted_Connection=True;TrustServerCertificate=true;";
     }
 }
