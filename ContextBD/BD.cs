f3aa42d0d05b2d791ddf69fe4dd337b3494b475d
UMGProyect <atreyuznight@gmail.com>
2024-07-28 08:38:28 -0600

    Se modifico las lineas de codigo en el storedhash para eliminar los espacios en blanco.

--- ContextBD/BD.cs
+++ ContextBD/BD.cs
@@ -3,6 +3,6 @@
     public class BD
     {
         //Conexion a la Base de Datos SQL Server
-        public static string connectionString = "Server=PCQ3_SERVER; Initial Catalog = SQ_BD; Trusted_Connection=True;TrustServerCertificate=true;";
+        public static string connectionString = "Server=; Initial Catalog = ; Trusted_Connection=True;TrustServerCertificate=true;";
     }
 }
