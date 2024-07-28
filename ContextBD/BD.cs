e991fe835b8f3075720fce06f65c860042303e34
UMGProyect <atreyuznight@gmail.com>
2024-07-28 07:46:03 -0600

    Se edito el comentario de la linea 5.

--- ContextBD/BD.cs
+++ ContextBD/BD.cs
@@ -2,7 +2,7 @@
 {
     public class BD
     {
-        //Conexion a la Base de datos SQL Server
+        //Conexion a la Base de Datos SQL Server
         public static string connectionString = "Server=PCQ3_SERVER; Initial Catalog = SQ_BD; Trusted_Connection=True;TrustServerCertificate=true;";
     }
 }
