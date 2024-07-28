37cd0e5a0094bcdbe5323c9aeba43c2d8ffa96be
Keyla321 <kaguilarl2@miumg.edu.gt>
2024-07-27 21:57:28 -0600

    Update user.cs
    Se agrego un comentario en la linea de codigo 5.

--- Models/User.cs
+++ Models/User.cs
@@ -2,6 +2,7 @@
 {
     public class User
     {
+        // modelo de usuario.
         public int Id { get; private set; }
         public string? Name { get; set; }
         public string? Password { get; set; }
