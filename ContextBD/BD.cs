a981dbb0e870c8d09f22cc1e51940e1abe1be746
UMGProyect <atreyuznight@gmail.com>
2024-09-19 22:08:26 -0600

    Conexion a BD en Azure establecida con exito

--- ContextBD/BD.cs
+++ ContextBD/BD.cs
@@ -3,6 +3,6 @@
     public class BD
     {
         //Conexion a la Base de Datos SQL Server
-        public static string connectionString = "Server=tcp:umgproyect.database.windows.net,1433;Initial Catalog=telecomunicaciones;Persist Security Info=False;User ID=adminit;Password={ProyectUMG32&};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+        public static string connectionString = "Server=tcp:umgproyect.database.windows.net,1433;Initial Catalog=telecomunicaciones;Persist Security Info=False;User ID=adminit;Password=ProyectUMG32&;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
     }
 }
