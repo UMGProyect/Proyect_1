0d7679bc8fe8b526ea13936c09d257b550b642dc
Darlin <djarlind@gmail.com>
2024-10-12 23:18:52 -0600

    Extrayendo

--- ContextBD/BD.cs
+++ ContextBD/BD.cs
@@ -3,6 +3,6 @@
     public class BD
     {
         //Conexion a la Base de Datos SQL Server
-        public static string connectionString = "Server=tcp:umgproyect.database.windows.net,1433;Initial Catalog=telecomunicaciones;Persist Security Info=False;User ID=adminit;Password=UmgProyect32&;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+        public static string connectionString = "Server=tcp:umgproyect.database.windows.net,1433;Initial Catalog=telecomunicaciones;Persist Security Info=False;User ID=adminit;Password=UmgProyect32;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;MultipleActiveResultSets=True;";
     }
 }
